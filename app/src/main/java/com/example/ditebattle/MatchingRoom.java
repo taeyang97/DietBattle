e5bff851e2b2892c5c1b313eb346c1fe3d019ce1
dltmdrb1996 <dltmdrb1996@naver.com>
2021-01-08 15:20:57 +0900

    내전화번호

---
+++ app/src/main/java/com/example/ditebattle/MatchingRoom.java
@@ -0,0 +1,15 @@
+package com.example.ditebattle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching_room);
+        // 01041959026
+    }
+}
