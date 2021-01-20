60d90502349c631b53dec22f20442cde84310dc0
jeongks <kisuk.jeong.lds@gmail.com>
2021-01-20 15:49:31 +0900

    user 테이블 객체 추가

---
+++ app/src/main/java/com/example/ditebattle/User.java
@@ -0,0 +1,52 @@
+package com.example.ditebattle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    public String id;
+    public String password;
+    public String name;
+    public String nickname;
+    public Integer age;
+    public String phone;
+    public Double weight;
+    public Double height;
+    public Double bmi;
+    public Integer total_point;
+    public Integer current_point;
+    public String gender;
+
+    public User(String id, String password, String name, String nickname, int age, String phone, double weight ,double height, double bmi, int total_point, int current_point, String gender){
+        this.id= id;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.age = age;
+        this.phone = phone;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+        this.total_point = total_point;
+        this.current_point = current_point;
+        this.gender = gender;
+
+    }
+
+    public Map<String, Object> toMap(){
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("password", password);
+        result.put("name",name);
+        result.put("nickname",nickname);
+        result.put("age",age);
+        result.put("phone",phone);
+        result.put("weight",weight);
+        result.put("height",height);
+        result.put("bmi", bmi);
+        result.put("total_point",total_point);
+        result.put("current_point",current_point);
+        result.put("gender",gender);
+        return result;
+    }
+}
